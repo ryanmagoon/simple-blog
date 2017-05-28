14733bf838a90b188a04e32113cb7cfed771956e
Ryan Magoon <mr.magoon5@gmail.com>
2017-05-28 18:52:45 -0400

    setup index route

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import { BrowserRouter, Route } from 'react-router-dom'
+import PostsIndex from './components/posts-index'
 
-import App from './components/app';
-import reducers from './reducers';
+import App from './components/app'
+import reducers from './reducers'
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware()(createStore)
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route path="" component={PostsIndex} />
+      </div>
+    </BrowserRouter>
   </Provider>
-  , document.querySelector('.container'));
+  , document.querySelector('.container'))
