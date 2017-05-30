90a8a074c4d291d983a71eaa7cf1cb061d127cd2
Ryan Magoon <mr.magoon5@gmail.com>
2017-05-30 01:11:25 -0400

    start building out new post form

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
-import { BrowserRouter, Route } from 'react-router-dom'
-import PostsIndex from './components/posts-index'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import ReduxPromise from 'redux-promise'
 
 import reducers from './reducers'
+import PostsIndex from './components/posts-index'
+import PostsNew from './components/posts-new'
 
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore)
 
@@ -14,7 +15,10 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path="" component={PostsIndex} />
+        <Switch>
+          <Route path="/posts/new" component={PostsNew} />
+          <Route path="/" component={PostsIndex} />
+        </Switch>
       </div>
     </BrowserRouter>
   </Provider>
