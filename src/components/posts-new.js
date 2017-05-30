7509e010427e611e85b08265cd22834a5ce8e749
Ryan Magoon <mr.magoon5@gmail.com>
2017-05-30 12:12:35 -0400

    sort out form validation

--- src/components/posts-new.js
+++ src/components/posts-new.js
@@ -3,21 +3,33 @@ import { Field, reduxForm } from 'redux-form'
 
 class PostsNew extends Component {
   renderField(field) {
+    const { meta: {touched, error}} = field
+    const className = `form-group ${touched && error ? 'has-danger' : ''}`
+
     return (
-      <div className="form-group">
+      <div className={className}>
         <label>{field.label}</label>
         <input
           className="form-control"
           type="text"
           { ...field.input }
         />
+      <div className="text-help">
+          {touched ? error : ''}
+        </div>
       </div>
     )
   }
 
+  onSubmit(values) {
+    console.log(values)
+  }
+
   render() {
+    const { handleSubmit } = this.props
+
     return (
-      <form action="">
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <Field
           label="Title"
           name="title"
@@ -33,11 +45,29 @@ class PostsNew extends Component {
           name="content"
           component={this.renderField}
         />
+      <button type="submit" className="btn btn-primary">Submit</button>
       </form>
     )
   }
 }
 
+function validate(values) {
+  const errors = {}
+
+  if(!values.title) {
+    errors.title = 'Enter a title!'
+  }
+  if(!values.categories) {
+    errors.categories = 'Enter some categories!'
+  }
+  if(!values.content) {
+    errors.content = 'Enter some content!'
+  }
+
+  return errors
+}
+
 export default reduxForm({
+  validate,
   form: 'PostsNewForm'
 })(PostsNew)
