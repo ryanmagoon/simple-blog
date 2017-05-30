163d9db2f3321fa58a2846e7c57aa4595f657dda
Ryan Magoon <mr.magoon5@gmail.com>
2017-05-30 02:38:01 -0400

    building out new post form, validation is next

--- src/components/posts-new.js
+++ src/components/posts-new.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react'
-imoprt { Field, reduxForm } from 'redux-form'
+import { Field, reduxForm } from 'redux-form'
 
 class PostsNew extends Component {
+  renderField(field) {
+    return (
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input
+          className="form-control"
+          type="text"
+          { ...field.input }
+        />
+      </div>
+    )
+  }
+
   render() {
     return (
       <form action="">
         <Field
+          label="Title"
           name="title"
-          component={}
+          component={this.renderField}
+        />
+        <Field
+          label="Categories"
+          name="categories"
+          component={this.renderField}
+        />
+        <Field
+          label="Post Content"
+          name="content"
+          component={this.renderField}
         />
       </form>
     )
